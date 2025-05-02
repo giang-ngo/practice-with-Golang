148226deea971e51cd576d29be1d4a83e5bbeaf7
giang-ngo <giangngo.py@gmail.com>
2025-05-02 18:08:25 +0700

    Practicing basic concepts in Golang

---
+++ practice2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	args := os.Args[1:]
+
+	guess, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Not a number.")
+		return
+	}
+
+	if guess < 0 {
+		fmt.Println("Please pick a positive number.")
+		return
+	}
+}
